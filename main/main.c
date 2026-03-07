474867e07a0084668cefba5ba6a8c14b8a4f37bb
Mervyn Wong <mjlwong@live.com.sg>
2026-03-08 01:53:27 +0800

    First working draft of code that updates IoT cloud date from sensor

--- main/main.c
+++ main/main.c
@@ -24,9 +24,6 @@
 #include "sdkconfig.h"
 #include "sht3x.h"
 
-// Declare queue for communication between tasks
-QueueHandle_t task_queue;
-
 // Tag for log message
 static const char * TAG = "main";
 
@@ -40,16 +37,21 @@ static sht3x_t sht31_dev;
 esp_rmaker_device_t * sht31_device;
 #endif
 
+// Add callback function if configured to test Rainmaker IoT service
+#if defined CONFIG_BOTH_RAINMAKER_AND_SENSOR || defined CONFIG_RAINMAKER_ONLY
+
 // Callback to handle commands received from the RainMaker cloud
 static esp_err_t write_cb(const esp_rmaker_device_t *device, const esp_rmaker_param_t *param,
             const esp_rmaker_param_val_t val, void *priv_data, esp_rmaker_write_ctx_t *ctx)
 {
+    // If there is text, display it
     if (ctx)
     {
         ESP_LOGI(TAG, "Received write request via : %s", esp_rmaker_device_cb_src_to_str(ctx->src));
     }
 
-    if (strcmp(esp_rmaker_param_get_name(param), "Dryness Level") == 0)
+    // If the user pressed the replaced silica gel button, display corresponding text
+    if (strcmp(esp_rmaker_param_get_name(param), "Press to indicate Silica Gel is replaced") == 0)
     {
         ESP_LOGI(TAG, "Silica Gel Replaced");
     }
@@ -57,14 +59,13 @@ static esp_err_t write_cb(const esp_rmaker_device_t *device, const esp_rmaker_pa
     return ESP_OK;
 }
 
+#endif
+
 // SHT31 task function
-void sht31_task(void * pvParameters)
+void sht31_init(void)
 {
     // Run code if configured to use SHT31 sensor
     #if defined CONFIG_BOTH_RAINMAKER_AND_SENSOR || defined CONFIG_SENSOR_ONLY
-
-    // Declare floats for temperature and humidity
-    float temperature_humidity[2];
 
     // Begin I2C
     ESP_ERROR_CHECK(i2cdev_init());
@@ -80,42 +81,17 @@ void sht31_task(void * pvParameters)
                                     CONFIG_I2C_MASTER_SCL));
     ESP_ERROR_CHECK(sht3x_init(&sht31_dev));
 
-    while(1)
-    {
-        // Measure the temperature and humidity value from the SHT31 sensor
-        ESP_ERROR_CHECK(sht3x_measure(&sht31_dev, &temperature_humidity[0], &temperature_humidity[1]));
-        // Send humidity data to queue
-        xQueueSend(task_queue, (void *) &temperature_humidity, (TickType_t) 5);
-        // Delay f0r 50ms
-        vTaskDelay(500/portTICK_PERIOD_MS);
-    }
-
-    #else // Run code if sensor not used, feed dummy 5.0 value to humidity
-    
-    float temperature_humidity[2] = {25.0, 50.0};
-
-    while(1)
-    {
-        // Send humidity data to queue
-        xQueueSend(task_queue, (void *) &temperature_humidity, (TickType_t) 5);
-        // Delay f0r 1min
-        vTaskDelay(60000/portTICK_PERIOD_MS);
-    }
-
     #endif
 }
 
 // Main function
 void app_main(void)
 {
-    // Float variable to store temperature and humidity
-    float temperature_humidity[2];
-
-    // Initialise queue for data transfer between tasks
-    task_queue = xQueueCreate(2, sizeof(temperature_humidity));
+    // Float variables to store temperature and humidity
+    float temperature = 25.0, humidity = 40.0;
 
     // Start task to receive data from SHT31 sensor
-    xTaskCreatePinnedToCore(sht31_task, "SHT31 Sensor Task", 8192, NULL, 2, NULL, 1);
+    sht31_init();
 
     // Run code if configured to test Rainmaker IoT service
     #if defined CONFIG_BOTH_RAINMAKER_AND_SENSOR || defined CONFIG_RAINMAKER_ONLY
@@ -138,14 +114,15 @@ void app_main(void)
 
     // Create Rainmaker node
     esp_rmaker_node_t * node = esp_rmaker_node_init(&rainmaker_cfg, "ESP RainMaker Device", "Drybox Device");
-    if (!node) {
+    if (!node)
+    {
         ESP_LOGE(TAG, "Could not initialise node. Aborting!!!");
         vTaskDelay(5000/portTICK_PERIOD_MS);
         abort();
     }
 
     // Initialise SHT31 device in Rainmaker application
-    sht31_device = esp_rmaker_device_create("Drybox Humidity Sensor", NULL, &temperature_humidity);
+    sht31_device = esp_rmaker_device_create("Drybox Humidity Sensor", NULL, NULL);
     esp_rmaker_device_add_cb(sht31_device, write_cb, NULL);
     esp_rmaker_node_add_device(node, sht31_device);
 
@@ -197,13 +174,7 @@ void app_main(void)
     For more information on the various ways of setting timezone, please check
     https://rainmaker.espressif.com/docs/time-service.html.
     */
-/*     esp_rmaker_timezone_service_enable();
-
-    // Enable scheduling.
-    esp_rmaker_schedule_enable();
-
-    // Enable Scenes
-    esp_rmaker_scenes_enable(); */
+    ESP_ERROR_CHECK(esp_rmaker_timezone_service_enable());
 
     // Enable Insights. Requires CONFIG_ESP_INSIGHTS_ENABLED=y
     ESP_ERROR_CHECK(app_insights_enable());
@@ -218,29 +189,37 @@ void app_main(void)
     after a connection has been successfully established
     */
     err = app_network_start(POP_TYPE_RANDOM);
-    if (err != ESP_OK) {
+    if (err != ESP_OK)
+    {
         ESP_LOGE(TAG, "Could not start Wifi. Aborting!!!");
         vTaskDelay(5000/portTICK_PERIOD_MS);
         abort();
     }
 
-    #else // Run code if not testing IoT, prints out data from queue instead
+    #endif
 
     while(1)
     {
-        // Check if the queue is not empty
-        if(task_queue != 0)
-        {
-            // Dequeue recent message
-            if(xQueueReceive(task_queue, &temperature_humidity, (TickType_t) 10))
-            {
-                // Print recent message
-                ESP_LOGI(TAG, "Temperature: %f C, Humidity: %f%%", temperature_humidity[0], temperature_humidity[1]);
-            }
-        }   
-        // Delay for 500ms
-        vTaskDelay(500/portTICK_PERIOD_MS);
-    }
+        // Run code if configured to use SHT31 sensor
+        #if defined CONFIG_BOTH_RAINMAKER_AND_SENSOR || defined CONFIG_SENSOR_ONLY
 
-    #endif
+        // Get measurement from sensor
+        ESP_ERROR_CHECK(sht3x_measure(&sht31_dev, &temperature, &humidity));
+
+        #endif
+
+        // Run code if configured to test Rainmaker IoT service
+        #if defined CONFIG_BOTH_RAINMAKER_AND_SENSOR || defined CONFIG_RAINMAKER_ONLY
+
+        // Add code to update rainmaker cloud here
+        ESP_ERROR_CHECK(esp_rmaker_param_update_and_report(humidity_param, esp_rmaker_float(humidity)));
+        ESP_ERROR_CHECK(esp_rmaker_param_update_and_report(temperature_param, esp_rmaker_float(temperature)));
+
+        #endif
+
+        ESP_LOGI(TAG, "Temperature(C): %f, Humidity(%%): %f", temperature, humidity);
+
+        // Delay for 1 min
+        vTaskDelay(60000/portTICK_PERIOD_MS);
+    }
 }
